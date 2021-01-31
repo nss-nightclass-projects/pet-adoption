f33f848ce5375ca24aca5758d0bf3756f14e8748
marceljones <marceljones.mj@gmail.com>
2021-01-31 16:23:42 -0600

    update links for loop

--- main.js
+++ main.js
@@ -11,14 +11,14 @@ const pets = [
     color: "Poop-Colored",
     specialSkill: "Just picks the tomatoes off of a sandwich instead of requesting a whole new sandwich.",
     type: "dino",
-    imageUrl: "http://www.jozilife.co.za/wp-content/uploads/The-Dino-Expo.jpg"
+    imageUrl: "https://mydinosaurs.com/wp-content/uploads/2017/02/2-3-600x400.jpg"
   },
   {
     name: "Whiskers",
     color: "Yellow",
     specialSkill: "Can prove he is a real man by drinking whiskey.",
-    type: "dino",
-    imageUrl: "https://mydinosaurs.com/wp-content/uploads/2017/02/2-3-600x400.jpg"
+    type: "cat",
+    imageUrl: "https://i.pinimg.com/originals/04/54/92/0454926d39eeb420f4f01948e94e9e41.jpg"
   },
   {
     name: "Coco",
@@ -53,7 +53,7 @@ const pets = [
     color: "Grey",
     specialSkill: "Comfortable in the outdoors for up to eight hours.",
     type: "dino",
-    imageUrl: "http://www.theouthousers.com/images/jck//ThanosCopter/news/grumpasaur.jpg"
+    imageUrl: "https://mydinosaurs.com/wp-content/uploads/2017/02/2-3-600x400.jpg"
   },
   {
     name: "Sassy",
@@ -108,8 +108,8 @@ const pets = [
     name: "Smokey",
     color: "Poop-Colored",
     specialSkill: "Drives at a safe rate of speed in snow or rain.",
-    type: "dino",
-    imageUrl: "https://images.readwrite.com/wp-content/uploads/2018/03/t-rex-dino-quiz-e1490854556549.jpg"
+    type: "dog",
+    imageUrl: "http://cdn.akc.org/content/article-body-image/funny-pembroke_welsh_corgi.jpg"
   },
   {
     name: "Muffin",
@@ -119,18 +119,18 @@ const pets = [
     imageUrl: "http://www.funnycatsite.com/pictures/Close_Up_Yawn.jpg"
   },
   {
-    name: "Salem",
+    name: "Shittz-Creek",
     color: "Poop-Colored",
     specialSkill: "Proficient in air guitar",
-    type: "dino",
-    imageUrl: "https://www.nation.co.ke/image/view/-/4078922/highRes/1742693/-/maxw/600/-/1453yvh/-/DINO.jpg"
+    type: "dog",
+    imageUrl: "https://www.thetrendywhippet.co.uk/wp-content/uploads/2016/11/IMG_1651-600x400.jpg"
   },
   {
     name: "Callie",
     color: "Blue",
     specialSkill: "Listens attentively to boring stories.",
     type: "dog",
-    imageUrl: "http://dailynewsdig.com/wp-content/uploads/2014/03/Creative-And-Funny-Dog-Stock-Photography-Pictures-2.jpg"
+    imageUrl: "https://www.thetrendywhippet.co.uk/wp-content/uploads/2016/11/IMG_1651-600x400.jpg"
   },
   {
     name: "Spooky",
@@ -143,15 +143,15 @@ const pets = [
     name: "Miss kitty",
     color: "Red",
     specialSkill: "Owns a Nintendo Power Glove.",
-    type: "dino",
-    imageUrl: "https://img.buzzfeed.com/buzzfeed-static/static/2015-11/2/12/enhanced/webdr15/anigif_enhanced-29802-1446485228-10.gif?crop=250:165;0,0&downsize=715"
+    type: "cat",
+    imageUrl: "http://3.bp.blogspot.com/-RzIFLsIO-XQ/UFoMLOT66JI/AAAAAAAAVps/JRF98hdA9S8/s640/funny-cat-pictures-016-027.jpg"
   },
   {
     name: "Snuggles",
     color: "Orange",
     specialSkill: "Is comfortable with jokes about his receding hairline.",
     type: "cat",
-    imageUrl: "http://funnyanimalphoto.com/wp-content/uploads/2013/08/cat_caught_mouse_thegatewaypundit.jpg"
+    imageUrl: "https://i.pinimg.com/originals/9d/da/3e/9dda3e5fd2b9886fc3d13ee47f52e8a0.jpg"
   },
   {
     name: "Buddy",
@@ -172,7 +172,7 @@ const pets = [
     color: "Red",
     specialSkill: "Knows the words to 4 rap songs.",
     type: "cat",
-    imageUrl: "http://funbk.s3.amazonaws.com/wp-content/uploads/2016/06/funny-cat-video-which-will-make-you-laugh-louder.jpg"
+    imageUrl: "https://i.pinimg.com/originals/9d/da/3e/9dda3e5fd2b9886fc3d13ee47f52e8a0.jpg"
   },
   {
     name: "Bubba",
@@ -206,8 +206,8 @@ const pets = [
     name: "Lucy",
     color: "Red",
     specialSkill: "Doesn’t get weirded out by the word “moist.”",
-    type: "dino",
-    imageUrl: "http://lsae2.iypcdn.com/static//modules/uploads/photos/language1/dino-live-22.jpg?119"
+    type: "dog",
+    imageUrl: "http://cdn.akc.org/content/article-body-image/funny-pembroke_welsh_corgi.jpg"
   }
 ];
 
